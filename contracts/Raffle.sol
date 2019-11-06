f1408ef1965bda62e4301c8d5e381302a0be31d2
DiegoGR97 <diegogr_97@hotmail.com>
2019-11-06 13:59:11 -0600

    Now able to buy tickets.

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -15,6 +15,7 @@ contract Raffle {
     struct Ticket{
         uint id;
         uint ticketNumber;
+        bool ticketTaken;
     }
     uint public competitorsCount;
     uint public ticketsCount;
@@ -27,6 +28,8 @@ contract Raffle {
     constructor() public {
         addRaffleCreator(1, 0x3D4921D9c0C9C07914E4F7006012d7E8CE1c5836, '415735b1fd4ca68e3f6d9cd36844fb61d560364a4c1f0d2ded23867165fba1ce');
         generateTicket('415735b1fd4ca68e3f6d9cd36844fb61d560364a4c1f0d2ded23867165fba1ce');
+        generateTicket('415735b1fd4ca68e3f6d9cd36844fb61d560364a4c1f0d2ded23867165fba1ce');
+
     }
 
     function addRaffleCreator (
@@ -41,11 +44,26 @@ contract Raffle {
         string memory privateKeyCreator = rafflecreator[1].privateKey;
         if (compareStrings(_privateKey,privateKeyCreator)){
             ticketsCount++;
-            tickets[ticketsCount] = Ticket(ticketsCount, ticketsCount);
+            tickets[ticketsCount] = Ticket(ticketsCount, ticketsCount, false);
         }
     }
     function compareStrings (string memory a, string memory b) public view returns (bool) {
-    return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))) );
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))) );
        }
-        
+
+    function buyTicket (
+    address _publicKey) public {
+        bool ticketAssigned = false;
+        for (uint i = 1; i <= ticketsCount; i++){
+            Ticket storage pivotTicket = tickets[i];
+            if (!ticketAssigned){
+                if (!(pivotTicket.ticketTaken)){
+                    competitorsCount++;
+                    competitors[competitorsCount] = Competitor(competitorsCount, _publicKey, pivotTicket.id);
+                    pivotTicket.ticketTaken = true;
+                    ticketAssigned = true;
+                }
+            }
+        }
+    }
 }
